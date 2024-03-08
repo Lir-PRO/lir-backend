eaded766cea0a9dd14f092e7935667be0385d017
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-03-08 13:40:27 +0100

    update user and user roles services

---
+++ Lir.Core/Interfaces/IUserRolesService.cs
@@ -0,0 +1,9 @@
+﻿using Lir.Core.Models;
+
+namespace Lir.Core.Interfaces
+{
+    public interface IUserRolesService : IEntityBaseRepository<UserRoles>
+    {
+       Task<IEnumerable<UserRoles>> GetByUserId(Guid userId);
+    }
+}
---
+++ lir.Infrastructure/Services/UserRolesService.cs
@@ -0,0 +1,22 @@
+﻿using Lir.Core.Interfaces;
+using Lir.Core.Models;
+using Lir.Infrastructure.Persistence;
+using Lir.Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lir.Infrastructure.Services
+{
+    public class UserRolesService : EntityBaseRepository<UserRoles>, IUserRolesService
+    {
+        private readonly LirDbContext _context;
+        public UserRolesService(LirDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<UserRoles>> GetByUserId(Guid userId)
+        {
+            return await _context.UserRoles.Where(ur => ur.UserId == userId).ToListAsync();
+        }
+    }
+}
