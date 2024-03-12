4d2a5adffb3d4a6c09eaa1186b5791a410676807
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-03-12 11:46:58 +0100

    add authentication service

---
+++ lir.Api/Authentication/TokenSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Lir.Api.Authentication
+{
+    public class TokenSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int AccessTokenExpirationMinutes { get; set; }
+        public int RefreshTokenExpirationDays { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
