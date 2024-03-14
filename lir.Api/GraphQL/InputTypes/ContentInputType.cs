816fc967d800341f42ac8a4afaae8ef416fc9bee
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-03-14 13:05:32 +0100

    add post and content input types

---
+++ lir.Api/GraphQL/InputTypes/ContentInputType.cs
@@ -0,0 +1,10 @@
+﻿using Lir.Core.Enums;
+
+namespace Lir.Api.GraphQL.InputTypes
+{
+    public class ContentInputType
+    {
+        public ContentType ContentType { get; set; }
+        public string ContentBase64 { get; set; }
+    }
+}
