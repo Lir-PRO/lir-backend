bfa82afde676b69afec583ad04e503b24f9ff799
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-03-14 13:06:16 +0100

    add AddCategory and AddPost mutations

--- lir.Api/GraphQL/Mutation/Mutation.cs
+++ lir.Api/GraphQL/Mutation/Mutation.cs
@@ -1,6 +1,8 @@
 ﻿using Lir.Api.Authentication;
 using Lir.Api.GraphQL.InputTypes;
 using Lir.Api.GraphQL.Payload;
+using Lir.Core.Interfaces;
+using Lir.Core.Models;
 
 namespace Lir.Api.GraphQL.Mutation
 {
@@ -28,6 +30,57 @@ namespace Lir.Api.GraphQL.Mutation
                 ConfirmPassword = passwordConfirmation
             };
             return await authenticationService.Register(input);
+        }
+
+        public async Task<Post> AddPost(AddPostInput input,
+            [Service] IPostService postService,
+            [Service] IContentService contentService,
+            [Service] IPostCategoryService postCategoryService)
+        {
+            var post = new Post
+            {
+                UserId = input.UserId,
+                Caption = input.Caption
+            };
+
+            await postService.AddAsync(post);
+
+            foreach (var categoryId in input.CategoryIds)
+            {
+                var postCategory = new PostCategory()
+                {
+                    PostId = post.Id,
+                    CategoryId = categoryId
+                };
+                await postCategoryService.AddAsync(postCategory);
+            }
+
+            foreach (var contentInput in input.ContentInputs)
+            {
+                var content = new Content()
+                {
+                    PostId = post.Id,
+                    ContentBase64 = contentInput.ContentBase64,
+                    ContentType = contentInput.ContentType
+                };
+
+                await contentService.AddAsync(content);
+            }
+
+            await postService.UpdateAsync(post.Id, post);
+            return post;
+        }
+
+        public async Task<Category> AddCategory(string name, string description, [Service] ICategoryService categoryService)
+        {
+            var category = new Category()
+            {
+                Name = name,
+                Description = description
+            };
+            await categoryService.AddAsync(category);
+
+            return category;
         }
     }
 }
