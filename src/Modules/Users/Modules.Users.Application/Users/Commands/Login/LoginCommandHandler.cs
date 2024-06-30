a02ec009216301b19c90191f64f9b8bc4542f1f9
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-06-30 11:34:21 +0200

    add login command

---
+++ src/Modules/Users/Modules.Users.Application/Users/Commands/Login/LoginCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Modules.Users.Application.Common.Interfaces;
+
+namespace Modules.Users.Application.Users.Commands.Login;
+
+public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
+{
+    private readonly IAuth0Service _auth0Service;
+
+    public LoginCommandHandler(IAuth0Service auth0Service)
+    {
+        _auth0Service = auth0Service;
+    }
+
+    public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        return await _auth0Service.LoginUser(request.Email, request.Password);
+    }
+}
