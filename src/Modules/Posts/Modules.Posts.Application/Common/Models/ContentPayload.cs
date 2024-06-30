8d6c47253b5ed266e8d50db01255d0e35a8c8ce1
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-07-01 00:14:07 +0200

    fix content payload model

--- src/Modules/Posts/Modules.Posts.Application/Common/Models/ContentPayload.cs
+++ src/Modules/Posts/Modules.Posts.Application/Common/Models/ContentPayload.cs
@@ -1,10 +1,11 @@
 ﻿using AutoMapper;
+using Modules.Posts.Application.Common.Mapping;
 using Modules.Posts.Domain.Entities;
 using Modules.Posts.Domain.Enums;
 
 namespace Modules.Posts.Application.Common.Models;
 
-public class ContentPayload
+public class ContentPayload : IMapFrom<Content>
 {
     public Guid Id { get; set; }
     public string Base64 { get; set; }
