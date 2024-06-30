f6cd255368ac09b466b0d5f3611679be3d33da28
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-06-30 11:34:46 +0200

    add user mutation

---
+++ src/Modules/Users/Modules.Users.Endpoints/GraphQL/Mutation/UserMutation.cs
@@ -0,0 +1,20 @@
+﻿using HotChocolate;
+using MediatR;
+using Modules.Users.Application.Common.Models;
+using Modules.Users.Application.Users.Commands.AddUser;
+using Modules.Users.Application.Users.Commands.Login;
+
+namespace Modules.Users.Endpoints.GraphQL.Mutation;
+
+public class UserMutation
+{
+    public async Task<string> AddUser(AddUserInput input, [Service] ISender mediatr)
+    {
+        return await mediatr.Send(new AddUserCommand(input));
+    }
+
+    public async Task<string> Login(string email, string password, [Service] ISender mediatr)
+    {
+        return await mediatr.Send(new LoginCommand(email, password));
+    }
+}
