ce4df3f6c9a416c1bcbe7f792fa8b251e6f44c9c
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-07-01 20:55:28 +0200

    add chats db initializer

---
+++ src/Modules/Chats/Modules.Chats.Persistence/ChatDbContextInitializer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace Modules.Chats.Persistence;
+
+public class ChatDbContextInitializer
+{
+    private readonly ILogger<ChatDbContextInitializer> _logger;
+    private readonly ChatContext _context;
+
+    public ChatDbContextInitializer(ILogger<ChatDbContextInitializer> logger,
+        ChatContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+    public async Task InitialiseAsync()
+    {
+        try
+        {
+            if (_context.Database.IsNpgsql())
+            {
+                await _context.Database.MigrateAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+
+    public async Task SeedAsync()
+    {
+        try
+        {
+            await TrySeedAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+
+    public async Task TrySeedAsync()
+    {
+    }
+}
