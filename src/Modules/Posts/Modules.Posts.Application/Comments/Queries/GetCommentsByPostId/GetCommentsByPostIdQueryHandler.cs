890ca04e03ea4bcadd9a73bb305533d3a211a4af
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-07-04 19:28:11 +0200

    add get comments by post id query

---
+++ src/Modules/Posts/Modules.Posts.Application/Comments/Queries/GetCommentsByPostId/GetCommentsByPostIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Modules.Posts.Application.Common.Models;
+using Modules.Posts.Domain.Interfaces;
+
+namespace Modules.Posts.Application.Comments.Queries.GetCommentsByPostId;
+
+public class GetCommentsByPostIdQueryHandler : IRequestHandler<GetCommentsByPostIdQuery, IQueryable<CommentPayload>>
+{
+    private readonly ICommentRepository _commentRepository;
+    private readonly IMapper _mapper;
+
+    public GetCommentsByPostIdQueryHandler(ICommentRepository commentRepository, IMapper mapper)
+    {
+        _commentRepository = commentRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IQueryable<CommentPayload>> Handle(GetCommentsByPostIdQuery request, CancellationToken cancellationToken)
+    {
+        var comments = await _commentRepository.GetCommentsByPostIdAsync(request.PostId);
+
+        var result = new List<CommentPayload>();
+
+        foreach (var comment in comments)
+        {
+            result.Add(_mapper.Map<CommentPayload>(comment));
+        }
+
+        return result.AsQueryable();
+    }
+}
--- src/Modules/Posts/Modules.Posts.Endpoints/ConfigureServices.cs
+++ src/Modules/Posts/Modules.Posts.Endpoints/ConfigureServices.cs
@@ -15,12 +15,14 @@ public static class ConfigureServices
         await services.AddPostsPersistenceServices(configuration);
         services.AddPostsApplicationServices(configuration);
         services.AddPostsInfrastructureServices(configuration);
-
-        services.AddScoped<PostQuery>();
-        services.AddScoped<CategoryQuery>();
+        
         services.AddScoped<PostMutation>();
         services.AddScoped<CategoryMutation>();
         services.AddScoped<CommentMutation>();
+
+        services.AddScoped<PostQuery>();
+        services.AddScoped<CategoryQuery>();
+        services.AddScoped<CommentQuery>();
 
         return services;
     }
