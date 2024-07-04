9332577c65e6fb2f7eb7fd2db93dd8a47ffdca6f
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-07-04 18:59:18 +0200

    add update comment mutation

---
+++ src/Modules/Posts/Modules.Posts.Application/Comments/Commands/UpdateComment/UpdateCommentCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Modules.Posts.Application.Common.InputTypes;
+using Modules.Posts.Application.Common.Models;
+
+namespace Modules.Posts.Application.Comments.Commands.UpdateComment;
+
+public record UpdateCommentCommand(UpdateCommentInput Input) : IRequest<CommentPayload>;
--- src/Modules/Posts/Modules.Posts.Endpoints/GraphQL/Mutations/CommentMutation.cs
+++ src/Modules/Posts/Modules.Posts.Endpoints/GraphQL/Mutations/CommentMutation.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Modules.Posts.Application.Comments.Commands.AddComment;
 using Modules.Posts.Application.Comments.Commands.DeleteComment;
+using Modules.Posts.Application.Comments.Commands.UpdateComment;
 using Modules.Posts.Application.Common.InputTypes;
 using Modules.Posts.Application.Common.Models;
 
@@ -17,5 +18,10 @@ public class CommentMutation
     public async Task<bool> DeleteComment(Guid id, [Service] ISender mediatr)
     {
         return await mediatr.Send(new DeleteCommentCommand(id));
+    }
+
+    public async Task<CommentPayload> UpdateComment(UpdateCommentInput input, [Service] ISender mediatr)
+    {
+        return await mediatr.Send(new UpdateCommentCommand(input));
     }
 }
