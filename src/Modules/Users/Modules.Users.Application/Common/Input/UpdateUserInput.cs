3c8150e2f3a36ddb24d815d66b3f7ea5d4b4e118
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-07-10 16:38:42 +0200

    add update user mutation

---
+++ src/Modules/Users/Modules.Users.Application/Common/Input/UpdateUserInput.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modules.Users.Application.Common.Input;
+
+public class UpdateUserInput
+{
+    public string Id { get; set; }
+    [Required(ErrorMessage = "Username is required")]
+    public string Username { get; set; }
+    public string? Name { get; set; }
+    public string? Bio { get; set; }
+    public string? ProfilePictureBase64 { get; set; }
+}
--- src/Modules/Users/Modules.Users.Endpoints/GraphQL/Mutations/UserMutation.cs
+++ src/Modules/Users/Modules.Users.Endpoints/GraphQL/Mutations/UserMutation.cs
@@ -1,8 +1,10 @@
 ﻿using HotChocolate;
 using MediatR;
 using Modules.Users.Application.Common.Input;
+using Modules.Users.Application.Common.Payload;
 using Modules.Users.Application.Users.Commands.AddUser;
 using Modules.Users.Application.Users.Commands.Login;
+using Modules.Users.Application.Users.Commands.UpdateUser;
 
 namespace Modules.Users.Endpoints.GraphQL.Mutations;
 
@@ -11,6 +13,11 @@ public class UserMutation
     public async Task<string> AddUser(AddUserInput input, [Service] ISender mediatr)
     {
         return await mediatr.Send(new AddUserCommand(input));
+    }
+
+    public async Task<UserPayload> UpdateUser(UpdateUserInput input, [Service] ISender mediatr)
+    {
+        return await mediatr.Send(new UpdateUserCommand(input));
     }
 
     public async Task<string> Login(string email, string password, [Service] ISender mediatr)
