288574a73fd4d5d9b1dcbb6e8011b9690a3d9e92
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-07-12 11:50:18 +0200

    add user deleted event and consumers for it

---
+++ src/Modules/Chats/Modules.Chats.Infrastructure/Consumers/UserDeletedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Common.Events;
+using MassTransit;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using Modules.Chats.Persistence;
+
+namespace Modules.Chats.Infrastructure.Consumers;
+
+public class UserDeletedConsumer : IConsumer<UserDeletedEvent>
+{
+    private readonly ChatContext _context;
+
+    public UserDeletedConsumer(ChatContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Consume(ConsumeContext<UserDeletedEvent> context)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == context.Message.UserId);
+
+        EntityEntry entityEntry = _context.Entry(user);
+        entityEntry.State = EntityState.Deleted;
+
+        await _context.SaveChangesAsync();
+    }
+}
