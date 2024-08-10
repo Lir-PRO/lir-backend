9f252827b8f36c77ec0b3dab3c44d2aedff4a6f0
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-10 17:19:36 +0200

    add error and response models

---
+++ src/Modules/Posts/Modules.Posts.Application/Common/Errors/CommentErrors.cs
@@ -0,0 +1,20 @@
+﻿namespace Modules.Posts.Application.Common.Errors;
+
+public static class CommentErrors
+{
+    public static readonly Error CommentIdRequired = new(
+        "Comments.CommentIdRequired",
+        "Comment Id is required");
+
+    public static readonly Error NullInput = new(
+        "Comments.NullInput",
+        "Input can not be null");
+
+    public static readonly Error NoContent = new(
+        "Comments.NoInput",
+        "Content can not be empty");
+
+    public static readonly Error NotFound = new(
+        "Comment.NotFound",
+        "Comment not found");
+}
---
+++ src/Modules/Posts/Modules.Posts.Application/Common/Errors/PostErrors.cs
@@ -0,0 +1,12 @@
+﻿namespace Modules.Posts.Application.Common.Errors;
+
+public static class PostErrors
+{
+    public static readonly Error UserIdRequired = new(
+        "Posts.UserIdRequired",
+        "User Id is required to create a post");
+
+    public static readonly Error NullInput = new(
+        "Posts.NullInput",
+        "Input can not be null");
+}
