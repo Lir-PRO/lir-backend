d5c55e64037807a96824d85540e938ab2862e769
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-10 17:25:12 +0200

    update posts commands

--- src/Modules/Posts/Modules.Posts.Application/Posts/Commands/AddPost/AddPostCommand.cs
+++ src/Modules/Posts/Modules.Posts.Application/Posts/Commands/AddPost/AddPostCommand.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
+using Modules.Posts.Application.Common;
 using Modules.Posts.Application.Common.InputTypes;
 using Modules.Posts.Application.Common.Models;
 
 namespace Modules.Posts.Application.Posts.Commands.AddPost
 {
-    public record AddPostCommand(AddPostInput Input) : IRequest<PostPayload>;
+    public record AddPostCommand(AddPostInput Input) : IRequest<Response<PostPayload>>;
 }
--- src/Modules/Posts/Modules.Posts.Application/Posts/Commands/UpdatePost/UpdatePostCommand.cs
+++ src/Modules/Posts/Modules.Posts.Application/Posts/Commands/UpdatePost/UpdatePostCommand.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
+using Modules.Posts.Application.Common;
 using Modules.Posts.Application.Common.InputTypes;
 using Modules.Posts.Application.Common.Models;
 
 namespace Modules.Posts.Application.Posts.Commands.UpdatePost
 {
-    public record UpdatePostCommand(UpdatePostInput Input) : IRequest<PostPayload>;
+    public record UpdatePostCommand(UpdatePostInput Input) : IRequest<Response<PostPayload>>;
 }
