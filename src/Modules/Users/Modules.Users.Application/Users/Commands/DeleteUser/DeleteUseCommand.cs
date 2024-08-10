60a08ffeca30b3a72ef7746f8f00099f1380e344
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-10 15:46:26 +0200

    update delete user command

--- src/Modules/Users/Modules.Users.Application/Users/Commands/DeleteUser/DeleteUseCommand.cs
+++ src/Modules/Users/Modules.Users.Application/Users/Commands/DeleteUser/DeleteUseCommand.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
+using Modules.Users.Application.Common;
 
 namespace Modules.Users.Application.Users.Commands.DeleteUser;
 
-public record DeleteUserCommand(string Id) : IRequest<bool>;
+public record DeleteUserCommand(string Id) : IRequest<Response<bool>>;
--- src/Modules/Users/Modules.Users.Application/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ src/Modules/Users/Modules.Users.Application/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,11 +1,12 @@
 ﻿using Common.Events;
 using MassTransit;
 using MediatR;
+using Modules.Users.Application.Common.Errors;
 using Modules.Users.Application.Common.Interfaces;
 using Modules.Users.Domain.Interfaces;
 
 namespace Modules.Users.Application.Users.Commands.DeleteUser;
-public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, Common.Response<bool>>
 {
     private readonly IUserRepository _userRepository;
     private readonly IPublishEndpoint _publishEndpoint;
@@ -18,20 +19,29 @@ public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
         _auth0Service = auth0Service;
     }
 
-    public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    public async Task<Common.Response<bool>> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
     {
-        if (!await _auth0Service.DeleteUser(request.Id))
+        try
         {
-            return false;
+            if (!await _auth0Service.DeleteUser(request.Id))
+            {
+                return  Common.Response<bool>.Failure(UserErrors.DeleteUserFailureAuth0);
+            }
+
+            var result = await _userRepository.DeleteAsync(request.Id, cancellationToken);
+
+            if (result)
+            {
+                await _publishEndpoint.Publish(new UserDeletedEvent(request.Id));
+            }
+
+            return result
+                ? Common.Response<bool>.Success(result)
+                : Common.Response<bool>.Failure(UserErrors.DeleteUserFailure);
         }
-
-        var result = await _userRepository.DeleteAsync(request.Id, cancellationToken);
-
-        if (result)
+        catch (Exception ex)
         {
-            await _publishEndpoint.Publish(new UserDeletedEvent(request.Id));
+            return Common.Response<bool>.Failure(UserErrors.DeleteUserFailure);
         }
-
-        return result;
     }
 }
