42484974c68b8cc74db6afa36d52ec77b3d409e4
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-10 17:26:24 +0200

    update comment mutation and query

--- src/Modules/Posts/Modules.Posts.Endpoints/GraphQL/Queries/CommentQuery.cs
+++ src/Modules/Posts/Modules.Posts.Endpoints/GraphQL/Queries/CommentQuery.cs
@@ -2,6 +2,7 @@
 using HotChocolate.Types;
 using MediatR;
 using Modules.Posts.Application.Comments.Queries.GetCommentsByPostId;
+using Modules.Posts.Application.Common;
 using Modules.Posts.Application.Common.Models;
 
 namespace Modules.Posts.Endpoints.GraphQL.Queries;
@@ -10,7 +11,7 @@ public class CommentQuery
     [UsePaging(IncludeTotalCount = true)]
     [UseFiltering]
     [UseSorting]
-    public async Task<IQueryable<CommentPayload>> GetCommentsByPostId(Guid postId, [Service] ISender mediatr)
+    public async Task<Response<IQueryable<CommentPayload>>> GetCommentsByPostId(Guid postId, [Service] ISender mediatr)
     {
         return await mediatr.Send(new GetCommentsByPostIdQuery(postId));
     }
