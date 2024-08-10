c6b3f268e53d27e0bc5b52be3d9e2157bcd03a11
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-10 17:21:11 +0200

    update add comment command

--- src/Modules/Posts/Modules.Posts.Application/Comments/Commands/AddComment/AddCommentCommand.cs
+++ src/Modules/Posts/Modules.Posts.Application/Comments/Commands/AddComment/AddCommentCommand.cs
@@ -1,7 +1,8 @@
 ﻿using MediatR;
+using Modules.Posts.Application.Common;
 using Modules.Posts.Application.Common.InputTypes;
 using Modules.Posts.Application.Common.Models;
 
 namespace Modules.Posts.Application.Comments.Commands.AddComment;
 
-public record AddCommentCommand(AddCommentInput Input) : IRequest<CommentPayload>;
+public record AddCommentCommand(AddCommentInput Input) : IRequest<Response<CommentPayload>>;
