9d15f4e9589da5233dea561dd30b9483269b4f21
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-10 15:45:48 +0200

    add error and response types

---
+++ src/Modules/Users/Modules.Users.Application/Common/Errors/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace Modules.Users.Application.Common.Errors;
+
+public class Error
+{
+    public Error(string code, string? description = null)
+    {
+        Code = code;
+        Description = description;
+    }
+    public string Code { get; set; }
+    public string? Description { get; set; } = null;
+    public static readonly Error None = new(string.Empty, string.Empty);
+}
---
+++ src/Modules/Users/Modules.Users.Application/Common/Errors/UserErrors.cs
@@ -0,0 +1,37 @@
+﻿namespace Modules.Users.Application.Common.Errors;
+
+public static class UserErrors
+{
+    public static readonly Error UsernameIsTaken = new(
+        "Users.UsernameIsTaken",
+        "User with this username already exists");
+
+    public static readonly Error EmailIsAlreadyUsed = new(
+        "Users.EmailIsAlreadyUsed",
+        "This email is already used by another account");
+
+    public static readonly Error AddUserFailure = new(
+        "Users.AddUserFailure",
+        "Failed to add the user");
+
+    public static readonly Error UpdateUserFailure = new(
+        "Users.UpdateUserFailure",
+        "Failed to update the user");
+
+    public static readonly Error InputIsNull = new(
+        "Users.InputIsNull",
+        "Input can not be null");
+
+    public static readonly Error NotFound = new(
+        "Users.NotFound",
+        "User not found");
+
+    public static readonly Error DeleteUserFailureAuth0 = new(
+        "Users.DeleteUserFailureAuth0",
+        "Failed to delete user from Auth0");
+
+    public static readonly Error DeleteUserFailure = new(
+        "Users.DeleteUserFailure",
+        "Failed to delete user");
+
+}
