c32370e14e743b8f50b0aa5976b31ef6ca9819ab
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-15 12:19:51 +0200

    add AddChatCommand

---
+++ src/Modules/Chats/Modules.Chats.Application/Chats/Commands/AddChat/AddChatCommand.cs
@@ -0,0 +1,6 @@
+﻿using Common;
+using MediatR;
+
+namespace Modules.Chats.Application.Chats.Commands.AddChat;
+
+public record AddChatCommand(List<string> ParticipantsIds) : IRequest<Response<Guid>>;
---
+++ src/Modules/Chats/Modules.Chats.Application/Chats/Commands/AddChat/AddChatCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using MediatR;
+using Modules.Chats.Application.Common.Errors;
+using Modules.Chats.Domain.Interfaces;
+
+namespace Modules.Chats.Application.Chats.Commands.AddChat;
+
+public class AddChatCommandHandler : IRequestHandler<AddChatCommand, Response<Guid>>
+{
+    private readonly IChatRepository _chatRepository;
+    private readonly IUserRepository _userRepository;
+
+    public AddChatCommandHandler(IChatRepository chatRepository, IUserRepository userRepository)
+    {
+        _chatRepository = chatRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task<Response<Guid>> Handle(AddChatCommand request, CancellationToken cancellationToken)
+    {
+        foreach (var participantId in request.ParticipantsIds)
+        {
+            if (!await _userRepository.UserExists(participantId))
+            {
+                return Response<Guid>.Failure(ChatErrors.UserNotFound);
+            }
+        }
+
+        var chatId = await _chatRepository.AddAsync(request.ParticipantsIds);
+
+        return Response<Guid>.Success(chatId);
+    }
+}
