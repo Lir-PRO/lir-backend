3d0852e8fc3a992cdceee8cb0b5eff02e00fa321
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-08-15 12:21:54 +0200

    add errors

---
+++ src/Modules/Chats/Modules.Chats.Application/Common/Errors/ChatErrors.cs
@@ -0,0 +1,10 @@
+﻿using Common;
+
+namespace Modules.Chats.Application.Common.Errors;
+
+public static class ChatErrors
+{
+    public static readonly Error UserNotFound = new(
+        "Chats.UserNotFound",
+        "User with given Id not found");
+}
---
+++ src/Modules/Chats/Modules.Chats.Application/Common/Errors/MessageErrors.cs
@@ -0,0 +1,11 @@
+﻿
+using Common;
+
+namespace Modules.Chats.Application.Common.Errors;
+
+public static class MessageErrors
+{
+    public static readonly Error EmptyMessage = new(
+        "Messages.EmptyMessage",
+        "Can not send an empty message");
+}
